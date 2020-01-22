66f80db357c3f4d4b57076e97864b54c80d285dc
karthik pamidimukkala <karthiksai231@gmail.com>
2020-01-22 00:05:24 -0500

    Adding Repository

--- board/src/main/java/com/agileproject/board/domain/Project.java
+++ board/src/main/java/com/agileproject/board/domain/Project.java
@@ -1,4 +1,37 @@
 package com.agileproject.board.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+    private Date created_At;
+    private Date updated_At;
+
+    @PrePersist
+    protected void onCreate() {
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated_At = new Date();
+    }
 }
---
+++ board/src/main/java/com/agileproject/board/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.agileproject.board.repositories;
+
+import com.agileproject.board.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
